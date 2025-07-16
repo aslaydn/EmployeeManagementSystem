88c53415dea205ff12726f7a0c368b7c1b8d4fb2
Aslı Aydın <87315246+aslaydn@users.noreply.github.com>
2025-07-16 17:22:53 +0300

    first commit

---
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        // birin çoğa ilişkisi
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+
+    }
+}
